205f0bbb41d9b610b0b5d1de4d2578d4e21214b7
Ahmed Atwa <aatwa86@gmail.com>
2021-01-24 19:53:02 +0000

    update

--- catalog/Controllers/Information/Contact.php
+++ catalog/Controllers/Information/Contact.php
@@ -8,55 +8,12 @@ class Contact extends BaseController
     {
         $this->template->setTitle(lang('information/contact.heading_title'));
         
-        if (($this->request->getMethod() == 'post') && $this->validateForm()) {
-            $email = \Config\Services::email();
-
-            $email->setFrom($this->registry->get('config_email'), $this->registry->get('config_name'));
-            $email->setTo(html_entity_decode($this->request->getPost('name'), ENT_QUOTES, 'UTF-8'));
-
-            $email->setSubject(html_entity_decode(sprintf(lang('information/contact.email_subject'), $this->request->getPost('name')), ENT_QUOTES, 'UTF-8'));
-            $email->setMessage($this->request->getPost('enquiry'));
-            
-            if ($email->send()) {
-                $this->session->setFlashdata('success', lang('information/contact.text_success'));
-            } else {
-                $this->session->setFlashdata('error_warning', lang('en.error.error_form'));
-            }
-        }
-
         $data['breadcrumbs'] = [];
 
         $data['breadcrumbs'][] = [
             'text' => lang('text_home'),
             'href' => base_url('common/home')
         ];
-
-
-        if ($this->request->getPost('name')) {
-            $data['name'] = $this->request->getPost('name');
-        } else {
-            $data['name'] = '';
-        }
-
-        if ($this->request->getPost('email')) {
-            $data['email'] = $this->request->getPost('email');
-        } else {
-            $data['email'] = '';
-        }
-
-        if ($this->request->getPost('subject')) {
-            $data['subject'] = $this->request->getPost('subject');
-        } else {
-            $data['subject'] = '';
-        }
-
-        if ($this->request->getPost('inquiry')) {
-            $data['inquiry'] = $this->request->getPost('inquiry');
-        } else {
-            $data['inquiry'] = '';
-        }
-
-        $data['action'] = route_to('contact');
 
         $data['site']       = $this->registry->get('config_name');
         $data['address']    = nl2br($this->registry->get('config_address'));
@@ -72,20 +29,37 @@ class Contact extends BaseController
         $this->template->output('information/contact', $data);
     }
 
-    protected function validateForm()
+    public function send()
     {
-        if (! $this->validate([
+        $json = [];
+        if ($this->request->isAJAX() && ($this->request->getMethod() == 'post')) {
+            if (! $this->validate([
                 'name'    => 'required|min_length[3]|max_length[32]',
                 'email'   => 'required|valid_email',
+                'subject' => 'required',
                 'inquiry' => 'required|min_length[10]|max_length[300]',
             ])) {
-            $this->session->setFlashdata('error_warning', lang('en.error.error_form'));
-            return false;
+                //$json['error_warning'] = lang($this->locale . '.error_form');
+                $json['errors'] = $this->validator->getErrors();
+            }
+
+            if (! $json) {
+                $email = \Config\Services::email();
+
+                $email->setFrom($this->registry->get('config_email'), $this->registry->get('config_name'));
+                $email->setTo(html_entity_decode($this->request->getPost('name'), ENT_QUOTES, 'UTF-8'));
+
+                $email->setSubject(html_entity_decode(sprintf(lang('information/contact.email_subject'), $this->request->getPost('name')), ENT_QUOTES, 'UTF-8'));
+                $email->setMessage($this->request->getPost('enquiry'));
+            
+                if ($email->send()) {
+                    $json['success'] = lang('information/contact.text_success');
+                }
+            }
         }
 
-        return true;
+        return $this->response->setJSON($json);
     }
-
 
     //--------------------------------------------------------------------
 }
--- catalog/Views/default/information/contact.php
+++ catalog/Views/default/information/contact.php
@@ -27,9 +27,9 @@
 		</div>
 		<div class="col">
 			<section class="margin-bottom-60">
-				<h3 class="headline margin-top-15 margin-bottom-35">Any questions? Feel free to contact us!</h3>
-				<form enctype="multipart/form-data" method="post" action="<?php echo $action; ?>" id="form-location" accept-charset="utf-8"> 
-					<input type="hidden" name="<?= csrf_token() ?>" value="<?= csrf_hash() ?>" />
+				<h3 class="headline margin-top-15 margin-bottom-35">Any questions? Feel free to contact us! <br />
+				<small><?php echo $text_help; ?></small></h3>
+				<form enctype="multipart/form-data" id="form-contact-us" accept-charset="utf-8"> 
 					<div class="form-group row">
 						<label for="formGroupExampleInput" class="col-sm-2 col-form-label">Name</label>
 						<div class="col-sm-10">
@@ -37,7 +37,7 @@
 								<div class="input-group-prepend">
 									<span class="input-group-text" id="basic-addon1"><i class="icon-material-outline-account-circle"></i></span>
 								</div>
-								<input class="form-control" name="name" type="text" required="required" />
+								<input class="form-control" name="name" type="text" id="input-name" />
 							</div>
 						</div>
 					</div>
@@ -48,7 +48,7 @@
 								<div class="input-group-prepend">
 									<span class="input-group-text" id="basic-addon1"><i class="icon-material-outline-email"></i></span>
 								</div>
-								<input class="form-control" name="email" type="email" pattern="^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$" required="required" />
+								<input class="form-control" name="email" type="email" id="input-email" />
 
 							</div>
 						</div>
@@ -60,18 +60,18 @@
 								<div class="input-group-prepend">
 									<span class="input-group-text" id="basic-addon1"><i class="icon-material-outline-assignment"></i></span>
 								</div>
-								<input class="form-control" name="subject" type="text" required="required" />
+								<input class="form-control" name="subject" type="text" id="input-subject" />
 							</div>
 						</div>
 					</div>
 					<div class="form-group row">
 						<label for="formGroupExampleInput" class="col-sm-2 col-form-label">Message</label>
 						<div class="col-sm-10">
-							<textarea class="form-control" name="inquiry" cols="40" rows="7" spellcheck="true" required="required"></textarea>
+							<textarea class="form-control" name="inquiry" cols="40" rows="7" spellcheck="true" id="input-inquiry"></textarea>
 						</div>
 					</div>
 					<div class="mx-auto text-right">
-					<input type="submit" class="submit button margin-top-15" id="submit" value="Submit Message" />
+					<button type="button" class="submit button margin-top-15" id="button-contact-us">Send Message</button>
 				</div>
 				</form>
 			</section>
@@ -84,5 +84,45 @@
 <script src="catalog/default/javascript/infobox.min.js"></script>
 <script src="catalog/default/javascript/markerclusterer.js"></script>
 <script src="catalog/default/javascript/maps.js"></script>
-
 <?php echo $footer;?>
+
+<script type="text/javascript">
+	$('#button-contact-us').on('click', function() {
+		var node = this;
+		$.ajax({
+			url: 'information/contact/send',
+			headers: {
+              "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr('content'),
+              "X-Requested-With": "XMLHttpRequest"
+           },
+		   dataType: 'json',
+		   method: 'post',
+		   data:$("#form-contact-us").serialize(),
+		   beforeSend: function() {
+		   	$('.alert, .invalid-feedback').remove();
+		   	$('form-contact-us input').removeClass('is-invalid');
+		   	$(node).html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...');
+		   },
+		   complete: function() {
+			$(node).html('Send Message');
+		   },
+		   success: function(json) {
+		   	if (json['errors']) {
+		   		for (i in json['errors']) {
+		   			var ele = $('#input-' + i );
+		   			ele.addClass('is-invalid');
+		   			ele.after('<div class="invalid-feedback">' + json['errors'][i] + '</div>');
+		   		}
+		   		// $('#form-contact-us').before('<div class="alert alert-danger alert-dismissible fade show" role="alert"><i class="fas fa-exclamation-circle"></i> ' +json['error_warning'] + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
+		   	}
+
+		   	if (json['success']) {
+		   		$('#form-contact-us').before('<div class="alert alert-success alert-dismissible fade show" role="alert"><i class="fas fa-check-circle"></i> ' + json['success'] +'<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>')
+		   	}
+		   },
+		   error: function(xhr, ajaxOptions, thrownError) {
+		      alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+		   }
+		});
+	});
+</script>
